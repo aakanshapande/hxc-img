46143cb7a06378b58e846bc6a8e6e96a9a1e3256
Mukesh Kumar <mukeshkumarcharak@gmail.com>
2025-02-18 22:32:20 +0530

    caching added in site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/(.*)", // Applies to all pages
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=31536000, immutable", // Cache static assets for 1 year
+            },
+          ],
+        },
+        {
+          source: "/api/(.*)", // Apply caching to API responses
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=3600, must-revalidate", // API response cache for 1 hour
+            },
+          ],
+        },
+        {
+          source: "/_next/static/(.*)", // Cache Next.js static files
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=31536000, immutable",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
