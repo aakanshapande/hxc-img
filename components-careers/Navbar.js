77965fa6c8d206e0a4fab59c29ef8372dc30c24c
aakanshapande <aakanshapande23@gmail.com>
2025-07-08 17:17:56 +0530

    fixed some minor changes

--- components-careers/Navbar.js
+++ components-careers/Navbar.js
@@ -75,7 +75,7 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item"><Link href="/#home" className="nav-link">Homes</Link></li>
+            <li className="nav-item"><Link href="/#home" className="nav-link">Home</Link></li>
             <li className="nav-item"><Link href="/#services" className="nav-link">Services</Link></li>
             <li className="nav-item"><Link href="/#testimonials" className="nav-link">Testimonials</Link></li>
             <li className="nav-item"><Link href="/#faq" className="nav-link">FAQ</Link></li>
